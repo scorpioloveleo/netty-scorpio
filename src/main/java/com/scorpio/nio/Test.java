433242d669219900d9c54c784e1b34bc40df632d
qinxinyu <xinyu.qin@transsnet.com>
2021-05-28 10:50:32 +0800

    //

--- src/main/java/com/scorpio/nio/Test.java
+++ src/main/java/com/scorpio/nio/Test.java
@@ -10,4 +10,39 @@ public class Test {
         System.out.println(NettyRuntime.availableProcessors());
 
     }
+
+
+    public static int minDistance(String word1, String word2) {
+
+        //状态转移方程
+        // dp[x][y] = dp[x-1][y-1]   dp[x-1][y]+1  dp[x][y-1]+1
+        char[] word11 = word1.toCharArray();
+        char[] word22 = word2.toCharArray();
+        int x = word11.length;
+        int y = word22.length;
+        int[][] dp = new int[x+1][y+1];
+
+
+        for (int i = 1; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                //四种情况
+                if(word11[i] != word22[j]){
+                    //1.替换
+                    dp[i][j] =dp[i-1][j-1]+1;
+                    //2.删除
+
+                    //3.增加
+
+
+
+                    dp[i][j];
+                }else {
+                    //4.跳过
+                    dp[i][j++] = dp[i-1][j-1];
+                }
+            }
+        }
+
+
+    }
 }
